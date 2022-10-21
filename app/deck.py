aba1e9f8790c0ca60964d6cfeefcba643d02f787
Ben Griffith <bengriffith@outlook.com>
2022-10-20 20:49:45 -0500

    updates per 20221020 review and moved Card into Deck module

--- app/deck.py
+++ app/deck.py
@@ -1,23 +1,28 @@
 from dataclasses import dataclass, field
-from typing import List
-
-from .card import Card
 
 
 @dataclass(frozen=True)
+class Card:
+    suit: str
+    rank: str
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.suit}, {self.rank})"
+
+
+@dataclass
 class Deck:
 
-    def create_deck():
+    cards: list = field(default_factory=list)   
+
+    def __post_init__(self):
         suits = "clubs diamonds hearts spades".split()
-        cards = "2 3 4 5 6 7 8 9 10 J Q K A".split()
+        _cards = "2 3 4 5 6 7 8 9 10 J Q K A".split()
         deck = []
         for suit in suits:
-            for card in cards:
+            for card in _cards:
                 deck.append(Card(suit, card))
-
-        return deck
-
-    cards: List[Card] = field(default_factory=create_deck)
+        self.cards = deck
 
     def __repr__(self):        
         return f"{self.__class__.__name__}({', '.join(card.__repr__() for card in self.cards)})"
--- tests/test_deck.py
+++ tests/test_deck.py
@@ -2,20 +2,19 @@ from dataclasses import FrozenInstanceError
 
 import pytest
 
-from app.card import Card
-from app.deck import Deck
+from app.deck import Card, Deck
 
 DECK = (
-    "Deck((clubs, 2), (clubs, 3), (clubs, 4), (clubs, 5), (clubs, 6), " 
-        "(clubs, 7), (clubs, 8), (clubs, 9), (clubs, 10), (clubs, J), "
-        "(clubs, Q), (clubs, K), (clubs, A), (diamonds, 2), (diamonds, 3), "
-        "(diamonds, 4), (diamonds, 5), (diamonds, 6), (diamonds, 7), (diamonds, 8), "
-        "(diamonds, 9), (diamonds, 10), (diamonds, J), (diamonds, Q), (diamonds, K), "
-        "(diamonds, A), (hearts, 2), (hearts, 3), (hearts, 4), (hearts, 5), (hearts, 6), "
-        "(hearts, 7), (hearts, 8), (hearts, 9), (hearts, 10), (hearts, J), (hearts, Q), "
-        "(hearts, K), (hearts, A), (spades, 2), (spades, 3), (spades, 4), (spades, 5), "
-        "(spades, 6), (spades, 7), (spades, 8), (spades, 9), (spades, 10), (spades, J), "
-        "(spades, Q), (spades, K), (spades, A))"
+    "Deck(Card(clubs, 2), Card(clubs, 3), Card(clubs, 4), Card(clubs, 5), Card(clubs, 6), " 
+        "Card(clubs, 7), Card(clubs, 8), Card(clubs, 9), Card(clubs, 10), Card(clubs, J), "
+        "Card(clubs, Q), Card(clubs, K), Card(clubs, A), Card(diamonds, 2), Card(diamonds, 3), "
+        "Card(diamonds, 4), Card(diamonds, 5), Card(diamonds, 6), Card(diamonds, 7), Card(diamonds, 8), "
+        "Card(diamonds, 9), Card(diamonds, 10), Card(diamonds, J), Card(diamonds, Q), Card(diamonds, K), "
+        "Card(diamonds, A), Card(hearts, 2), Card(hearts, 3), Card(hearts, 4), Card(hearts, 5), Card(hearts, 6), "
+        "Card(hearts, 7), Card(hearts, 8), Card(hearts, 9), Card(hearts, 10), Card(hearts, J), Card(hearts, Q), "
+        "Card(hearts, K), Card(hearts, A), Card(spades, 2), Card(spades, 3), Card(spades, 4), Card(spades, 5), "
+        "Card(spades, 6), Card(spades, 7), Card(spades, 8), Card(spades, 9), Card(spades, 10), Card(spades, J), "
+        "Card(spades, Q), Card(spades, K), Card(spades, A))"
 )
 
 @pytest.fixture
@@ -31,7 +30,7 @@ def deck():
 def test_card(card):
     assert card.suit == "hearts"
     assert card.rank == "5"
-    assert card.__repr__() == "(hearts, 5)"
+    assert card.__repr__() == "Card(hearts, 5)"
 
 
 def test_card_mutability_error():
