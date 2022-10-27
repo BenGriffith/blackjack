0eac78ac395bcd4b863f1151d3c64548286721d8
Ben Griffith <bengriffith@outlook.com>
2022-10-26 21:13:51 -0500

    removed validation from constructor and updated hit to include dealer

--- app/action.py
+++ app/action.py
@@ -1,22 +1,16 @@
-
-
 class Action:
 
-    def __init__(self, action, deck, player):
+    def __init__(self, deck, player, dealer):
         self.deck = deck
         self.player = player
+        self.dealer = dealer
 
-        if not self.validate_action(action):
-            return
-
-    def validate_action(self, action):
-        if action not in ["hit", "stand", "double down"]:
-            print("Please choose either hit, stand or double down")
-        return True
-
-    def hit(self):
+    def hit(self, individual):
         card = self.deck.cards.pop()
-        self.player.hand = card
+        if individual == "player":
+            self.player.hand = card
+        else:
+            self.dealer.hand = card
         return
 
     def stand(self):
