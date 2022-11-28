009b40d227365bdfbd1e09dd45974708c8fc9055
Ben Griffith <bengriffith@outlook.com>
2022-11-27 21:18:25 -0600

    added fixtures and test cases

--- tests/test_game.py
+++ tests/test_game.py
@@ -12,12 +12,42 @@ def dealer():
     return blackjack.Dealer()
 
 @pytest.fixture
+def dealer_one_card(deck, dealer):
+    dealer.deal_card(deck, blackjack.Action)
+    return dealer
+
+@pytest.fixture
 def player():
     return blackjack.Player()
 
 @pytest.fixture
+def player_one_card(deck, player):
+    player.deal_card(deck, blackjack.Action)
+    return player
+
+@pytest.fixture
 def game(deck, dealer, player):
     return blackjack.Game(deck, dealer, player)
+
+@pytest.fixture()
+def first_round(player_one_card, dealer_one_card):
+    message = (
+        f"\nResult after Round 1\n"
+        "---------------\n"
+        f"{player_one_card}\n"
+        f"{dealer_one_card.__str__()}\n\n"
+    )
+    return message
+
+@pytest.fixture
+def final_round(player_one_card, dealer_one_card):
+    message = (
+        "\nFinal Result\n"
+        "---------------\n"
+        f"{player_one_card}\n"
+        f"{dealer_one_card}\n\n"
+    )
+    return message
 
 
 def test_deal_card_message(capsys, game, player, dealer):
@@ -29,20 +59,28 @@ def test_deal_card_message(capsys, game, player, dealer):
     assert captured.out == "Dealing Dealer Card...\n"
 
 
-def test_result_message(capsys, game, deck, player, dealer):
-    player.deal_card(deck, blackjack.Action)
-    dealer.deal_card(deck, blackjack.Action)
-
-    message = (
-        "\nResult after Round 1\n"
-        "---------------\n"
-        f"{player}\n"
-        f"{dealer.__str__()}\n\n"
-    )
-
-    game._result_message(True, dealer.__str__())
+def test_result_message(capsys, game, dealer_one_card, first_round, final_round):
+    game._result_message(True, dealer_one_card.__str__())
     captured = capsys.readouterr()
-    assert captured.out == message
+    assert captured.out == first_round
+
+    game._result_message()
+    captured = capsys.readouterr()
+    assert captured.out == final_round
+
+
+def test_player_action_hit(capsys, game):
+    game._player_action_hit()
+    captured = capsys.readouterr()
+    assert captured.out == f"Dealing Player Card...\n{game.player}\n"
+
+
+def test_player_action_stay_first_check(game):
+    game.dealer.score = 10
+    game.player.score = 9
+    assert game._player_action_stay() == None
+
+
 
 
 # def test_game_start(monkeypatch, game):
